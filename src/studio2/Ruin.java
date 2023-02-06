7a14b93c112a3bca7d3059f9036717b18d85f618
emmafeirstein <emmafeirstein@emmas-air.dhcp.wustl.edu>
2023-02-06 13:10:07 -0600

    Updated Studio

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -21,21 +21,22 @@ public class Ruin {
 		int winCount = 0;
 		int loseCount = 0;
 		// simulate one day
-		int dayNumber = 0; 
+		int dayNumber = 0;
 		int numberRuins = 0;
 		int numberWins = 0;
+
 		while (totalSimulations > 0) {
-			dayNumber ++; 
+			dayNumber++;
 			while (startAmount < winLimit && startAmount > 0) {
 				double randomNumber = Math.random();
-				
+
 				if (randomNumber <= winChance) {
 					startAmount++;
 					winCount++;
 
 					if (startAmount == winLimit) {
 						System.out.println("Simulation " + dayNumber + ": Winner!");
-						numberWins ++; 
+						numberWins++;
 					}
 
 				} else {
@@ -44,19 +45,19 @@ public class Ruin {
 
 					if (startAmount == 0) {
 						System.out.println("Simulation " + dayNumber + ": Ruin");
-						numberRuins ++; 
+						numberRuins++;
 					}
 
 				}
 
 			}
-			
+
 			totalSimulations--;
-			
+
 		}
 		System.out.println("Number of Plays:" + (winCount + loseCount));
 		System.out.println("Wins: " + numberWins + " Ruins: " + numberRuins);
-		System.out.println("Ruin rate: " + ((double)numberRuins/((double)winCount + loseCount)));
+		System.out.println("Ruin rate: " + ((double) numberRuins / ((double) winCount + loseCount)));
 	}
 
 }
